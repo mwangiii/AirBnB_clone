b9794725f8fdde95df4360063dad3b51b67f5565
mwangiii <mwangi8kevin@gmail.com>
2023-03-11 13:54:09 +0300

    console

--- console.py
+++ console.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-#import BaseModel
 import cmd
+
 
 class HBNBCommand(cmd.Cmd):
     # """class that in herits from cmd"""
@@ -45,17 +45,18 @@ class HBNBCommand(cmd.Cmd):
 
 #     def do_show(self, class_name):
 #         """Usage: show <class> <id> or <class>.show(<id>)
-#             Display the string representation of a class instance of a given id.
+#             Display the string representation of a class instance
+#             of a given id.
 #         """
 #         if class_name == "":
 #             print("**class name is missing**")
-    
+
 #         elif class_name not in HBNBCommand.__classes:
 #             print("**class doesn't exist**")
 
 #         elif id == "" :
 #             print("**instance id is missing**")
-    
+
 #         else:
 #             print("{} {}".format(class_name, id))
 # #def do_destroy(self, ):
