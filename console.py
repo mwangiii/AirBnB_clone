3a6460232fe3377c1e5d389ed06e3e4be309a0c4
mwangiii <mwangi8kevin@gmail.com>
2023-03-11 13:51:16 +0300

    console

--- console.py
+++ console.py
@@ -3,18 +3,18 @@
 import cmd
 
 class HBNBCommand(cmd.Cmd):
-    """class that in herits from cmd"""
+    # """class that in herits from cmd"""
 
-    prompt = "(hbnb)"
-    __classes = {
-        "BaseModel",
-        "User",
-        "State",
-        "City",
-        "Place",
-        "Amenity",
-        "Review"
-    }
+    # prompt = "(hbnb)"
+    # __classes = {
+    #     "BaseModel",
+    #     "User",
+    #     "State",
+    #     "City",
+    #     "Place",
+    #     "Amenity",
+    #     "Review"
+    # }
 
     def do_quit(self, arg):
 
@@ -29,36 +29,36 @@ class HBNBCommand(cmd.Cmd):
         """an empty line + ENTER shouldnt execute anything"""
         pass
 
-    def do_create(self, class_name):
-        """Creates an instance.
-        """
-        if class_name = "":
-            print("** class name missing **")
+    # def do_create(self, class_name):
+    #     """Creates an instance.
+    #     """
+    #     if class_name = "":
+    #         print("** class name missing **")
 
-        elif class_name not in HBNBCommand.__classes:
-            print("** class doesn't exist **")
-        else:
-            b = HBNBCommand.__classes.dict[class_name]()
-            b.save()
-            print(b.id)
+    #     elif class_name not in HBNBCommand.__classes:
+    #         print("** class doesn't exist **")
+    #     else:
+    #         b = HBNBCommand.__classes.dict[class_name]()
+    #         b.save()
+    #         print(b.id)
 
 
-    def do_show(self, class_name):
-        """Usage: show <class> <id> or <class>.show(<id>)
-            Display the string representation of a class instance of a given id.
-        """
-        if class_name == "":
-            print("**class name is missing**")
+#     def do_show(self, class_name):
+#         """Usage: show <class> <id> or <class>.show(<id>)
+#             Display the string representation of a class instance of a given id.
+#         """
+#         if class_name == "":
+#             print("**class name is missing**")
     
-        elif class_name not in HBNBCommand.__classes:
-            print("**class doesn't exist**")
+#         elif class_name not in HBNBCommand.__classes:
+#             print("**class doesn't exist**")
 
-        elif id == "" :
-            print("**instance id is missing**")
+#         elif id == "" :
+#             print("**instance id is missing**")
     
-        else:
-            print("{} {}".format(class_name, id))
-#def do_destroy(self, ):
+#         else:
+#             print("{} {}".format(class_name, id))
+# #def do_destroy(self, ):
 
 
 if __name__ == '__main__':
