f903b02dc3bc5315c54260d6d917fd3b82d3261d
mwangiii <mwangi8kevin@gmail.com>
2023-03-12 21:56:17 +0300

    trial

--- console.py
+++ console.py
@@ -4,7 +4,7 @@ import cmd
 
 class HBNBCommand(cmd.Cmd):
     """class that in herits from cmd"""
-
+    prompt = "(hbnb)"
     def do_quit(self, line):
         """Quit command to exit the program"""
         return True
@@ -16,3 +16,6 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """an empty line + ENTER shouldnt execute anything"""
         pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
