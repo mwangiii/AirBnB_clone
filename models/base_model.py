fe7649b1e9b870f3afedeccafd1f55f8f04b6745
Joan Ndegwa <44367535+jnjerin@users.noreply.github.com>
2023-03-13 03:22:02 +0300

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,1 +1,34 @@
+#!/usr/bin/python3
+"""This module defines a base class for all models in our AirBnB clone"""
+import uuid
+from datetime import datetime
 
+
+class BaseModel:
+    """Base model class for all other classes to inherit from"""
+    def __init__(self):
+        """Constructor method to initialize attributes"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        """Returns a string representation of the instance"""
+        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
+        return '[{}] ({}) {}'.format(cls, self.id, self.__dict__)
+
+    def save(self):
+        """Updates the public instance attribute updated_at with the current datetime"""
+        from models import storage
+        self.updated_at = datetime.now()
+        storage.save()
+
+    def to_dict(self):
+        """Returns a dictionary containing all keys/values of __dict__ of the instance"""
+        dictionary = {}
+        dictionary.update(self.__dict__)
+        dictionary.update({'__class__':
+                          (str(type(self)).split('.')[-1]).split('\'')[0]})
+        dictionary['created_at'] = self.created_at.isoformat()
+        dictionary['updated_at'] = self.updated_at.isoformat()
+        return dictionary
