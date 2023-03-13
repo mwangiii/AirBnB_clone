a94567042355bf0a42bc08b2ba375c3f1f19442a
Joan Ndegwa <44367535+jnjerin@users.noreply.github.com>
2023-03-13 03:32:33 +0300

    Update to allow passing of arguments

--- models/base_model.py
+++ models/base_model.py
@@ -6,11 +6,21 @@ from datetime import datetime
 
 class BaseModel:
     """Base model class for all other classes to inherit from"""
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Constructor method to initialize attributes"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+         if not kwargs:
+            from models import storage
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+            storage.new(self)
+        else:
+            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            del kwargs['__class__']
+            self.__dict__.update(kwargs)
 
     def __str__(self):
         """Returns a string representation of the instance"""
