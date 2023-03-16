2a4e003fcfe81ffc484a8038756732b9c80f8014
Joan Ndegwa <44367535+jnjerin@users.noreply.github.com>
2023-03-16 23:52:05 +0300

    Update with BaseModel

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,1 +1,18 @@
+#!/usr/bin/python3
+""" """
+from models.base_model import BaseModel
+import unittest
+import datetime
+from uuid import UUID
+import json
+import os
 
+
+class test_basemodel(unittest.TestCase):
+    """ """
+
+    def __init__(self, *args, **kwargs):
+        """ """
+        super().__init__(*args, **kwargs)
+        self.name = 'BaseModel'
+        self.value = BaseModel
